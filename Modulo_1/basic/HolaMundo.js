da59a122a7b163a83ce61fc7c4e7798d7ac48b01
Esteban Revelo <84397774+StbanJ@users.noreply.github.com>
2021-06-18 12:38:33 -0500

    Tarea Console
    Consultar los tipos de console

--- Modulo_1/basic/HolaMundo.js
+++ Modulo_1/basic/HolaMundo.js
@@ -1,1 +1,54 @@
 console.log("Hola mundo");
+
+/* Tarea Consultar las funciones de Console */ 
+
+// console.error()
+// Este método muestra un icono de error y el texto del mensaje en color rojo.
+
+// console.warn()
+// Este método muestra un icono de advertencia en color amarillo y el texto del mensaje.
+
+// console.dir()
+// Este método muestra una lista de árbol de las propiedades de un objeto JavaScript.
+
+// console.group()
+// Para agrupar resultados de la consola debemos incluirlos dentro de una mismo bloque de código, el cual se inicia con este!
+
+// console.groupEnd()
+// y finaliza con este!
+
+// console.groupCollapsed()
+// Crea un nuevo grupo de resultados de consola que está inicialmente colapsado
+
+// console.table()
+// Este método crea una tabla con la información de un objeto o array cuando estos contienen datos similares.
+
+// console.assert()
+// Este método es bastante útil cuando estamos realizando pruebas sencillas de nuestra aplicación, este
+// método consta de dos parámetros, el primero indica la condición y el segundo el mensaje de error que se
+// imprimirá en consola si la condición se evalúa como false.
+
+// console.count(label)
+// Esta método registra la cantidad de veces que se ha llamado count()
+
+// console.clear()
+// Limpia lo que se encuentre dentro del terminal
+
+// console.info()
+// Manda un mensaje de informacion 
+
+// console.debug()
+// Registra un mensaje con el nivel de"debug"
+
+// console._exception()
+// Es un alias de error
+
+// Console.time()
+// Inicia un temporizador con un nombre específico como parámetro. Hasta 10.000 temporizadores 
+// simultáneos pueden ejecutarse en una página determinada.
+
+// Console.timeEnd()
+// Detiene el temporizador especificado y registra el tiempo transcurrido, en segundos, desde su inicio.
+
+// Console.trace()
+// Muestra una traza de error.
