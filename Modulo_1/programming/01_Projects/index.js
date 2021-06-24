88767d233e2e21b2580e3db8c9eff555bc044845
Esteban Revelo <84397774+StbanJ@users.noreply.github.com>
2021-06-24 12:01:13 -0500

    Req y Ques routes
    Trabajo en Clases 6/24/2021

--- Modulo_1/programming/01_Projects/index.js
+++ Modulo_1/programming/01_Projects/index.js
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 // genero una aplicacion express
-const app = express()
+const app = express();
 // EndPoint
-app.get('/', (req, res) => {
-    res.send("Hola mundo con ExpressJs")
-})
+app.get("/", (req, res) => {
+  res.send("Hola mundo con ExpressJs");
+});
+
+app.get("/saludo", (req, res) => {
+  // req.query
+  //console.log(req.query);
+  // obtenemos la query del objeto request
+  const { query } = req;
+  // const nombre = req.query.nombre
+  // const apellido = req.query.apellido
+
+  res.json({
+    saludo: `Hola soy ${query.nombre} ${query.apellido}`,
+  });
+});
+
+app.get("/saludo/:nombre/", (req, res) => {
+  const {
+    params: { nombre },
+  } = req;
+  
+  res.json({
+    nombre: nombre,
+  });
+});
 
 app.listen(3000, () => {
-    console.log("Servidor escuchando en http://localhost:3000");
-})
+  console.log("Servidor escuchando en http://localhost:3000");
+});
---
+++ Modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,16 @@
+// traemos el paquete
+const {Router} = require('express');
+
+// Inicializamos Router
+const router = Router()
+
+// Creacion de los endpoints
+
+module.exports.RouterIndex = router
+
+
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
