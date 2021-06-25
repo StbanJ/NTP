2f875035f78dea201136add19ceb888324c152b4
Esteban Revelo <84397774+StbanJ@users.noreply.github.com>
2021-06-25 12:16:49 -0500

    Clase de rutas
    Trabajo  en clase 06/25/2021
    ROUTES

--- Modulo_1/programming/01_Projects/index.js
+++ Modulo_1/programming/01_Projects/index.js
@@ -2,33 +2,24 @@ const express = require("express");
 
 // genero una aplicacion express
 const app = express();
-// EndPoint
-app.get("/", (req, res) => {
-  res.send("Hola mundo con ExpressJs");
-});
 
-app.get("/saludo", (req, res) => {
-  // req.query
-  //console.log(req.query);
-  // obtenemos la query del objeto request
-  const { query } = req;
-  // const nombre = req.query.nombre
-  // const apellido = req.query.apellido
+// routes
 
-  res.json({
-    saludo: `Hola soy ${query.nombre} ${query.apellido}`,
-  });
-});
+const {RouterIndex} = require('./routes/index');
 
-app.get("/saludo/:nombre/", (req, res) => {
-  const {
-    params: { nombre },
-  } = req;
-  
-  res.json({
-    nombre: nombre,
-  });
-});
+app.use("/",RouterIndex)
+app.use("/user",RouterIndex)
+app.use("/user/data",RouterIndex)
+
+/*
+  "/" + "/" = "//" => "/"
+  "/" + "/saludo" = "//saludo" => "/saludo"
+  "/" + "/saludo/:nombre" => "/saludo/:nombre"
+
+  "/user" + "/" "//" => "/"
+  "/user" + "/saludo" = "/user/saludo"
+  "/user/data" + "/saludo" = "/user/data/saludo"
+*/
 
 app.listen(3000, () => {
   console.log("Servidor escuchando en http://localhost:3000");
--- Modulo_1/programming/01_Projects/routes/index.js
+++ Modulo_1/programming/01_Projects/routes/index.js
@@ -2,9 +2,37 @@
 const {Router} = require('express');
 
 // Inicializamos Router
+
 const router = Router()
 
-// Creacion de los endpoints
+// creacion de los endpoints
+router
+    .get("/", (req, res) => {
+        res.send("Hola mundo con ExpressJs");
+    })
+    .get("/saludo",(req, res) => {
+
+        //console.log(req.query);
+        const { query: {nombre, apellido} } = req;
+    
+        //const nombre = req.query.nombre;
+        //const apellido = req.query.apellido;
+    
+        res.json({
+            saludo: `Hola soy ${nombre} ${apellido}`
+        });
+    
+    })
+    .get('/saludo/:nombre', (req, res) => {
+
+        const { params: {nombre} } = req;
+        res.json({
+            nombre: nombre
+        })
+    });
+
+
+// Exportacion de los endpoints
 
 module.exports.RouterIndex = router
 
